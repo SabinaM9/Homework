77c7bc01796f0b35b28487be01b1a7f9afad5b99
Sabina Mussaeva <msabin8@mail.ru>
2020-09-22 17:34:27 +0300

    Сценарии для тестов, сессия Analasys, таблицы AnalasysSummary и Профиль, метрики, MNT

---
+++ Скрипты/06_Search_the_ticket/Action.c
@@ -0,0 +1,163 @@
+Action()
+{
+	lr_start_transaction("06_Search_the_ticket");
+
+	web_set_sockets_option("SSL_VERSION", "2&3");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"none");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"document");
+
+	web_add_auto_header("Sec-Fetch-Mode", 
+		"navigate");
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_start_transaction("connection");
+	
+//input type="hidden" name="userSession" value="129622.008991495zzzHtDcpAiHftAAAtpfQcfcf"/>
+	web_reg_save_param("userSession",
+		"LB=input type=\"hidden\" name\=\"userSession\" value\=\"",
+		"RB=\"\/\>",
+		LAST);
+
+	//Проверка транзакции connection
+	web_reg_find("Text=A Session ID has been created and loaded into a cookie called MSO.",
+		LAST);
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("connection", LR_AUTO);
+
+	web_revert_auto_header("Sec-Fetch-User");
+
+	web_add_auto_header("Sec-Fetch-Dest", 
+		"frame");
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_header("Origin", 
+		"http://localhost:1080");
+
+	web_add_auto_header("Sec-Fetch-Site", 
+		"same-origin");
+
+	lr_start_transaction("login");
+	
+	//Проверка транзакции login
+	web_reg_find("Text=Welcome, <b>{login}</b>, to the Web Tours reservation pages",
+		LAST,
+		"Text=User password was correct - added a cookie with the user's default",
+		LAST);
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={login}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=login.x", "Value=52", ENDITEM,
+		"Name=login.y", "Value=1", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		LAST);
+
+	lr_end_transaction("login", LR_AUTO);
+
+	web_add_auto_header("Sec-Fetch-User", 
+		"?1");
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	lr_think_time(6);
+
+	web_url("Search Flights Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_auto_header("Origin", 
+		"http://localhost:1080");
+
+	lr_think_time(22);
+
+	lr_start_transaction("insert_info_for_ticket_search");
+	
+	//Проверка транзакции insert_info_for_ticket_search
+	web_reg_find("Text=Flight departing from <B>{departure}</B> to <B>{arrival}</B> on <B>{departDate}</B>",
+		LAST);
+
+	web_submit_data("reservations.pl", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=depart", "Value={departure}", ENDITEM, 
+		"Name=departDate", "Value={departDate}", ENDITEM, 
+		"Name=arrive", "Value={arrival}", ENDITEM, 
+		"Name=returnDate", "Value={returnDate}", ENDITEM, 
+		"Name=numPassengers", "Value={numPassengers}", ENDITEM, 
+		"Name=seatPref", "Value={seatPref}", ENDITEM, 
+		"Name=seatType", "Value={seatType}", ENDITEM, 
+		"Name=findFlights.x", "Value=39", ENDITEM, 
+		"Name=findFlights.y", "Value=17", ENDITEM, 
+		"Name=.cgifields", "Value=roundtrip", ENDITEM, 
+		"Name=.cgifields", "Value=seatType", ENDITEM, 
+		"Name=.cgifields", "Value=seatPref", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("insert_info_for_ticket_search", LR_AUTO);
+
+	lr_think_time(28);
+
+	lr_start_transaction("logout");
+	
+	//Проверка транзакции logout
+	web_reg_find("Text=A Session ID has been created and loaded into a cookie called MSO.",
+		LAST);
+
+	web_url("welcome.pl", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Snapshot=t9.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("logout", LR_AUTO);
+	
+	lr_end_transaction("06_Search_the_ticket", LR_AUTO);
+
+}
